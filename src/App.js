b54054649632fa0391c57b54220efa156c9b91a9
ManLikeTeetos <tolu.mastermindz@gmail.com>
2023-06-07 19:44:39 +0100

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import LandingPage from 'pages/LandingPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,42 +1,110 @@
-import React from 'react';
-import { Box, Button, Flex, Heading, Image, Link, Text } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, Flex, Heading, Image, Link, Text } from '@chakra-ui/react';
+import banner from '../images/banner.jpg';
 
 function LandingPage() {
     return (
-        <Box>
-            {/* Header */}
-            <Flex align="center" justify="space-between" p={4}>
-                <Heading as="h1" fontSize="xl">
-                    Artfinder
-                </Heading>
-                <Flex>
-                    <Link mr={4}>Artworks</Link>
-                    <Link>Artists</Link>
-                </Flex>
-            </Flex>
+        <ChakraProvider>
+            <Box>
+                {/* Intro Section */}
+                <Box bg="gray.100">
+                    <Image src={banner} alt="Cover Image" />
+                    <Heading as="h1" fontSize="4xl" textAlign="center" mt={4}>
+                        Artfinder
+                    </Heading>
+                    <Text fontSize="xl" textAlign="center" my={4}>
+                        Discover and explore art from around the world.
+                    </Text>
+                    <Flex justify="center" mt={8}>
+                        <Button colorScheme="blue" size="lg">
+                            Explore Artfinder
+                        </Button>
+                    </Flex>
+                </Box>
 
-            {/* Cover Image */}
-            <Image src="path_to_cover_image.jpg" alt="Artfinder Cover" />
+                {/* Featured Section */}
+                <Box mt={16} mx={10}>
+                    <Heading as="h2" fontSize="3xl" textAlign="center">
+                        Featured
+                    </Heading>
+                    <Flex justify="space-around" mt={8}>
+                        {/* Feature 1 */}
+                        <Box>
+                            <Image src="/feature1-image.jpg" alt="Feature 1" />
+                            <Heading as="h3" fontSize="xl" mt={4}>
+                                Feature 1
+                            </Heading>
+                            <Text mt={2}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ligula sapien.
+                            </Text>
+                        </Box>
 
-            {/* Project Description */}
-            <Box p={4}>
-                <Text fontSize="lg" textAlign="center" my={4}>
-                    Discover and explore a diverse collection of artworks from around the world.
-                </Text>
+                        {/* Feature 2 */}
+                        <Box>
+                            <Image src="/feature2-image.jpg" alt="Feature 2" />
+                            <Heading as="h3" fontSize="xl" mt={4}>
+                                Feature 2
+                            </Heading>
+                            <Text mt={2}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ligula sapien.
+                            </Text>
+                        </Box>
+
+                        {/* Feature 3 */}
+                        <Box>
+                            <Image src="/feature3-image.jpg" alt="Feature 3" />
+                            <Heading as="h3" fontSize="xl" mt={4}>
+                                Feature 3
+                            </Heading>
+                            <Text mt={2}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ligula sapien.
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Box>
+
+                {/* About Section */}
+                <Box mt={16} mx={10}>
+                    <Heading as="h2" fontSize="3xl" textAlign="center">
+                        About
+                    </Heading>
+                    <Text mt={8}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ligula sapien. Phasellus eleifend
+                        lobortis justo, ac sollicitudin odio efficitur in. Vestibulum ante ipsum primis in faucibus orci luctus et
+                        ultrices posuere cubilia curae; Praesent dapibus gravida dapibus. Proin faucibus tincidunt nibh at
+                        ullamcorper. Donec tincidunt nulla ut dignissim convallis. Pellentesque iaculis libero justo, sed rutrum
+                        mi auctor nec.
+                    </Text>
+                    <Flex justify="center" mt={4}>
+                        <Link href="https://github.com/your-repo" color="blue.500" mr={4}>
+                            GitHub Repository
+                        </Link>
+                        <Link href="https://linkedin.com/your-profile" color="blue.500" mr={4}>
+                            LinkedIn
+                        </Link>
+                        <Link href="https://twitter.com/your-profile" color="blue.500">
+                            Twitter
+                        </Link>
+                    </Flex>
+                </Box>
+
+                {/* Video Section */}
+                <Box mt={16} mx={10}>
+                    <Heading as="h2" fontSize="3xl" textAlign="center">
+                        Project Video
+                    </Heading>
+                    <Box mt={8} textAlign="center">
+                        <iframe
+                            width="560"
+                            height="315"
+                            src="https://www.youtube.com/embed/your-video-id"
+                            title="Project Video"
+                            frameBorder="0"
+                            allowFullScreen
+                        ></iframe>
+                    </Box>
+                </Box>
             </Box>
-
-            {/* Button to Deployed Application */}
-            <Box textAlign="center" mt={4}>
-                <Button colorScheme="blue" size="lg" as={Link} href="link_to_deployed_application">
-                    Explore Artfinder
-                </Button>
-            </Box>
-
-            {/* Footer */}
-            <Box bg="gray.200" p={4} mt={8} textAlign="center">
-                &copy; 2023 Artfinder. All rights reserved.
-            </Box>
-        </Box>
+        </ChakraProvider>
     );
 }
 
