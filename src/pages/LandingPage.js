384fec7daa4f524c7877ce889a142a0ebb8c0b18
ManLikeTeetos <tolu.mastermindz@gmail.com>
2023-06-08 20:26:09 +0100

    Landing Page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,19 +1,20 @@
-import {Box, Button, ChakraProvider, Flex, Heading, Image, Link, Text} from '@chakra-ui/react';
+import {Box, Button, ChakraProvider, Flex, Heading, Image, Link, Text} from "@chakra-ui/react";
 import {useState, useEffect} from "react";
-import banner from '../images/banner.jpg';
-import signup from '../images/signup.jpg';
-import gallery from '../images/gallery.jpg';
-import update from '../images/update.jpg';
-import about from '../images/afgallery.png';
-import introData from '../introdata.json';
-import featuresData from '../featuresdata.json';
-import aboutData from '../aboutdata.json';
+import {HamburgerIcon} from "@chakra-ui/icons";
+import banner from "../images/banner.jpg";
+import signup from "../images/signup.jpg";
+import gallery from "../images/gallery.jpg";
+import update from "../images/update.jpg";
+import about from "../images/afgallery.png";
+import introData from "../introdata.json";
+import featuresData from "../featuresdata.json";
+import aboutData from "../aboutdata.json";
 
 function LandingPage() {
-
     const [features, setFeatures] = useState([]);
     const [aboutinfo, setAboutinfo] = useState([]);
     const [intro, setIntro] = useState([]);
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         // Simulate fetching features data from an API
@@ -22,41 +23,71 @@ function LandingPage() {
         setIntro(introData);
     }, []);
 
-
     return (
         <ChakraProvider>
             <Box>
                 {/* Menu Bar */}
-                <Flex align="center"
-                      justify="space-between"
-                      p={4}
-                      top={0}
-                      position="sticky"
-                      zIndex={1}
-                      bg="white"
+                <Flex
+                    align="center"
+                    justify="space-between"
+                    p={4}
+                    top={0}
+                    position="sticky"
+                    zIndex={1}
+                    bg="white"
                 >
                     <Heading as="h1" fontSize="xl">
                         {introData[0].name}
                     </Heading>
-                    <Flex>
-                        <Link href="#" fontSize="xl" mr={8} fontWeight="semibold"> Home </Link>
-                        <Link href="#features" fontSize="xl" mr={8} fontWeight="semibold"> Features </Link>
-                        <Link href="#about" fontSize="xl" mr={8} fontWeight="semibold"> About </Link>
-                        <Link href="#work" fontSize="xl" mr={{base: 0, md: 20}} fontWeight="semibold"> How It
-                            Works </Link>
+                    <Flex display={{base: "none", md: "flex"}}>
+                        <Link href="#" fontSize="xl" mr={8} fontWeight="semibold">
+                            Home
+                        </Link>
+                        <Link href="#features" fontSize="xl" mr={8} fontWeight="semibold">
+                            Features
+                        </Link>
+                        <Link href="#about" fontSize="xl" mr={8} fontWeight="semibold">
+                            About
+                        </Link>
+                        <Link href="#work" fontSize="xl" mr={{base: 0, md: 20}} fontWeight="semibold">
+                            How It Works
+                        </Link>
                     </Flex>
+                    <Box display={{base: "block", md: "none"}} onClick={() => setMenuOpen(!isMenuOpen)}>
+                        <HamburgerIcon boxSize={6}/>
+                    </Box>
                 </Flex>
+
+                {/* Responsive Menu */}
+                {isMenuOpen && (
+                    <Flex direction="column" p={4} bg="white">
+                        <Link href="#" fontSize="xl" my={2} fontWeight="semibold">
+                            Home
+                        </Link>
+                        <Link href="#features" fontSize="xl" my={2} fontWeight="semibold">
+                            Features
+                        </Link>
+                        <Link href="#about" fontSize="xl" my={2} fontWeight="semibold">
+                            About
+                        </Link>
+                        <Link href="#work" fontSize="xl" my={2} fontWeight="semibold">
+                            How It Works
+                        </Link>
+                    </Flex>
+                )}
+
                 {/* Intro Section */}
-                <Flex bg="gray.100" position="relative" alignItems="center" height="100vh">
+                <Flex bg="gray.100" position="relative" alignItems="center" minHeight="100vh">
                     <Box position="absolute" top={0} left={0} width="100%" height="100%">
                         <Image src={banner} alt="Cover Image" objectFit="cover" width="100%" height="100%"/>
                     </Box>
 
-                    <Box position="relative" zIndex={1} p={12} textAlign="left" color="black" width="40%" bg="white">
-                        <Heading as="h1" fontSize="4xl" mb={4}>
+                    <Box position="relative" zIndex={1} p={12} textAlign="left" color="black" width="100%"
+                         maxWidth="40rem" mx="auto">
+                        <Heading as="h1" fontSize={{base: "4xl", md: "6xl"}} mb={4}>
                             {introData[0].name}
                         </Heading>
-                        <Text fontSize="xl" mb={4} fontWeight="light">
+                        <Text fontSize={{base: "xl", md: "2xl"}} mb={4} fontWeight="light">
                             {introData[0].desc}
                         </Text>
                         <Button colorScheme="blue" size="lg" mt={12}>
@@ -71,11 +102,11 @@ function LandingPage() {
                         Art Finder Features
                     </Heading>
                     <Heading as="h1" fontSize="xl" textAlign="center" fontWeight="light">
-                        Here a some of the exciting features of ArtFinder
+                        Here are some of the exciting features of ArtFinder
                     </Heading>
                     <Flex direction="column" mt={8}>
                         {/* Feature 1 */}
-                        <Flex alignItems="start" height="400px" mb={24}>
+                        <Flex alignItems="start" flexDirection={{base: "column", md: "row"}} height="auto" mb={24}>
                             <Box flex="1">
                                 <Image src={signup} alt="Feature 1"/>
                             </Box>
@@ -83,30 +114,30 @@ function LandingPage() {
                                 <Heading as="h4" fontSize="3xl" mt={{base: 4, md: 12}}>
                                     {featuresData[0].name}
                                 </Heading>
-                                <Text mt={4} fontSize="xl" fontWeight="light">
+                                <Text mt={4} fontSize={{base: "xl", md: "2xl"}} fontWeight="light">
                                     {featuresData[0].description}
                                 </Text>
                             </Box>
                         </Flex>
 
                         {/* Feature 2 */}
-                        <Flex alignItems="start" height="400px" mb={24}>
+                        <Flex alignItems="start" flexDirection={{base: "column-reverse", md: "row"}} height="auto"
+                              mb={24}>
                             <Box flex="1" ml={{base: 0, md: 8}}>
                                 <Heading as="h3" fontSize="3xl" mt={4}>
                                     {featuresData[1].name}
                                 </Heading>
-                                <Text mt={4} fontSize="xl" fontWeight="light">
+                                <Text mt={4} fontSize={{base: "xl", md: "2xl"}} fontWeight="light">
                                     {featuresData[1].description}
                                 </Text>
                             </Box>
                             <Box flex="1">
                                 <Image src={gallery} alt="Feature 2"/>
                             </Box>
-
                         </Flex>
 
                         {/* Feature 3 */}
-                        <Flex alignItems="start" height="400px" mb={24}>
+                        <Flex alignItems="start" flexDirection={{base: "column", md: "row"}} height="auto" mb={24}>
                             <Box flex="1">
                                 <Image src={update} alt="Feature 3"/>
                             </Box>
@@ -114,7 +145,7 @@ function LandingPage() {
                                 <Heading as="h3" fontSize="3xl" mt={4}>
                                     {featuresData[2].name}
                                 </Heading>
-                                <Text mt={4} fontSize="xl" fontWeight="light">
+                                <Text mt={4} fontSize={{base: "xl", md: "2xl"}} fontWeight="light">
                                     {featuresData[2].description}
                                 </Text>
                             </Box>
@@ -122,27 +153,27 @@ function LandingPage() {
                     </Flex>
                 </Box>
 
-
                 {/* About Section */}
                 <Box mt={16} mx={10} textAlign="center">
-                    <Heading as="h2" fontSize="3xl" mb={4}>
+                    <Heading as="h2" fontSize={{base: '2xl', md: '3xl'}} mb={4}>
                         About ArtFinder
                     </Heading>
                 </Box>
-                <Box mt={16} mx={10} id="about" height="600px" display="flex" boxShadow="xl">
+                <Box mt={16} mx={10} id="about" minHeight="400px" display="flex"
+                     flexDirection={{base: 'column', md: 'row'}} boxShadow="xl">
                     {/* Image */}
-                    <Box flex="1">
-                        <Image src={about} alt="About Image" h="80%" objectFit="cover"/>
+                    <Box flex={{base: '1', md: '1'}}>
+                        <Image src={about} alt="About Image" h={{base: '40%', md: '80%'}} objectFit="cover"/>
                     </Box>
 
                     {/* Text */}
-                    <Box flex="1" p={8} textAlign="left">
+                    <Box flex={{base: '1', md: '1'}} p={8} textAlign="left">
                         {aboutData.map((item, index) => (
                             <div key={index}>
-                                <Heading as="h2" fontSize="2xl" >
+                                <Heading as="h2" fontSize={{base: 'xl', md: '2xl'}}>
                                     {item.name}
                                 </Heading>
-                                <Text my={4} fontWeight="light" fontSize="m">
+                                <Text my={4} fontWeight="light" fontSize={{base: 'sm', md: 'md'}}>
                                     {item.description}
                                 </Text>
                             </div>
@@ -161,31 +192,49 @@ function LandingPage() {
                     </Box>
                 </Box>
 
-
                 {/* Video Section */}
                 <Box id="work" mt={16} py={16} bg="#a52a2a8a" color="white">
-                    <Heading as="h2" fontSize="3xl" textAlign="center">
+                    <Heading as="h2" fontSize={{base: '2xl', md: '3xl'}} textAlign="center">
                         How it Works
                     </Heading>
-                    <Heading as="h1" fontSize="xl" textAlign="center" fontWeight="light">
+                    <Heading as="h1" fontSize={{base: 'lg', md: 'xl'}} textAlign="center" fontWeight="light">
                         Watch a quick video to see how Artfinder helps you navigate the Art World.
                     </Heading>
-                    <Box mt={16} textAlign="center" >
-                        <div style={{ display: 'flex', justifyContent: 'center' }}>
-                            <div style={{ position: 'relative', width: '70%', paddingBottom: '56.25%' }}>
-                                <iframe
-                                    src="https://www.loom.com/embed/1bb7f6053d644e449c0252c066d3f0c8"
-                                    title="Project Video"
-                                    frameBorder="0"
-                                    webkitallowfullscreen
-                                    mozallowfullscreen
-                                    allowFullScreen
-                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '70%' }}
-                                ></iframe>
-                            </div>
+                    <Box mt={16} textAlign="center">
+                        <div style={{position: 'relative', width: '100%', paddingBottom: '56.25%'}}>
+                            <iframe
+                                src="https://www.loom.com/embed/1bb7f6053d644e449c0252c066d3f0c8"
+                                title="Project Video"
+                                frameBorder="0"
+                                webkitallowfullscreen
+                                mozallowfullscreen
+                                allowFullScreen
+                                style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}
+                            ></iframe>
                         </div>
                     </Box>
                 </Box>
+                {/* Footer */}
+                <Box bg="#f8f8f8" py={4} px={6} borderTop="1px solid #ddd" mt={16}>
+                    <Flex justifyContent="center" alignItems="center">
+                        <Box>
+                            {/* Social Media Links */}
+                            <Link href="https://github.com/ManLikeTeetos" color="blue.500" mr={4}>
+                                GitHub
+                            </Link>
+                            <Link href="https://www.linkedin.com/in/ade-ojo-toluwanimi/" color="blue.500" mr={4}>
+                                LinkedIn
+                            </Link>
+                            <Link href="https://twitter.com/ManLikeTeetos" color="blue.500">
+                                Twitter
+                            </Link>
+                        </Box>
+                    </Flex>
+                    <Text fontSize="sm" textAlign="center" mt={2} color="gray.500">
+                        &copy; 2023 Your Name. All rights reserved. | Created by Ade-ojo Toluwanimi
+                    </Text>
+                </Box>
+
             </Box>
         </ChakraProvider>
     );
